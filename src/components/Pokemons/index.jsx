e04c5df7c45c91a3d1922aa5a50eb41f33025380
Marcos R. Morato <marcos.nyrou@hotmail.com>
2020-11-27 03:08:06 -0300

    favoritos pokemon funcionando

--- src/components/Pokemons/index.jsx
+++ src/components/Pokemons/index.jsx
@@ -1,18 +1,32 @@
 // voltar infoPokemon até pages e manipular la a condição de addFavorite
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { Card } from "../../components/styled-components/Card/index";
 
 const Pokemons = ({ url, addFavorite, favorite, removeFavorite }) => {
   const [infoPokemon, setInfoPokemon] = useState([]);
   const [favo, setFavo] = useState(false);
-  const [teste, setTeste] = [];
+  const location = useLocation();
 
   useEffect(() => {
     axios.get(`${url}`).then((res) => {
       setInfoPokemon(res.data);
     });
+    // console.log(location.pathname === "/pokemons");
   }, [url]);
+
+  useEffect(() => {
+    if (location.pathname === "/pokemons") {
+      // console.log(favorite.length);
+
+      const find = favorite?.find((e) => e.name === infoPokemon.name);
+      if (find !== undefined) {
+        console.log(find, "funciona?");
+        setFavo(true);
+      }
+    }
+  }, [infoPokemon]);
 
   const conditionAddFavorite = () => {
     if (!favo) {
